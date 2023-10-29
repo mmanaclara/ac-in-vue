a1da73d69ec306568ea69a1563d5ae026766c0b7
Clara Moreira <anacacamm@gmail.com>
2023-10-29 17:00:43 -0300

    creating home page

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background: url('/images/bg-pattern.png');
+  margin-inline: auto;
+}
+</style>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <main class="page-container grid grid-cols-2 items-center gap-4">
+    <div class="flex flex-col justify-center gap-6">
+      <h1 class="text-title-1">Let's create unforgettable memories together</h1>
+      <p class="text-zinc-600">
+        Explore the world, discover new cultures <br />
+        and expand your horizons.
+      </p>
+
+      <button class="btn-primary hover:opacity-90">Find out more</button>
+    </div>
+    <div class="w-[30rem] items-end ml-auto">
+      <img
+        src="/images/woman-holding-passport-in-one-hand-and-the-global-in-the-other.png"
+        alt=""
+        class="w-full object-cover"
+      />
+    </div>
+  </main>
+</template>
